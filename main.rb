9d23eae39c7858d632f670abde308ace3ee8e59a
Ogawa <135859092+ogawa3427@users.noreply.github.com>
2023-08-30 02:13:27 +0900

    何もわからん

--- main.rb
+++ main.rb
@@ -1,16 +1,33 @@
-#Rubyでで標準入力の第一引数を出力するプログラムを作成してください。
-#ただし、第一引数がない場合は、error!と出力してください。
-
-if ARGV[0] == nil
-    puts "error!"
-else
-    if ARGV[0] == "-r"
-        puts "regi mode"
-    elseif ARGV[0] == "-l"
-        puts "list mode"
-    elseif ARGV[0] == "-o"
-        puts "output mode"
-    else
-        puts "error!"
+def check_file_existence(file_path)
+    unless File.exist?(file_path)
+      puts "Error: File does not exist."
     end
 end
+
+require 'csv'
+
+#初期動作
+# 実行ファイルのディレクトリを取得
+current_dir = File.dirname(__FILE__)
+
+# kesuna.txtのフルパスを生成
+kesuna_file_path = File.join(current_dir, 'kesuna.txt')
+
+# kesuna.txtが存在するか確認
+if File.exist?(kesuna_file_path)
+    #puts "kesuna.txt is found."
+else
+    #puts "kesuna.txt is not found."
+    File.open('kesuna.txt', 'w') {}
+end
+
+if ARGV[0] == "-r"
+    file_path = ARGV[1]
+    if File.exist?(file_path)
+        File.open( 'kesuna.txt', 'w' ) { |file| file.puts ARGV[1] }
+    else
+        puts "Error: File does not exist."
+    end
+end
+
+
